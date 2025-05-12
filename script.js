b9dca9433132301eff02645af9b45f9e70622946
darshana-vyas2004 <darshanavyas2021@gmail.com>
2025-05-12 20:56:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function firstWord(s) {
-  // your code here
+    s = s.trimStart().trimEnd(); // Fix: reassign after trimming
+    let newarray = s.split(" ");
+    return newarray[0]; // Fix: return the first word
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
